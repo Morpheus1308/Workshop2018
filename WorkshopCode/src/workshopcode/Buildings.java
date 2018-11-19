686a7b66b3d4c199916ba35edfd7cf49a41640a9
Niclas Ryge <nikkeryge@live.dk>
2018-11-19 15:01:19 +0100

    Epic Hilter test

--- WorkshopCode/src/workshopcode/Buildings.java
+++ WorkshopCode/src/workshopcode/Buildings.java
@@ -1,10 +1,39 @@
 
 package workshopcode;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 /**
  *
  * @author bende
  */
-public class Buildings {
+public class Buildings {  
 
+    //ArrayAttributes
+    ArrayList<Building> buildingList = new ArrayList<>();
+
+    //Constructor
+    public Buildings() {
+    }
+    
+    //Methods
+    
+    @Override
+    public String toString() {
+        
+        String combinedString = "";
+        
+        for (Building b : buildingList) {
+           combinedString += b.toString();
+        }
+        
+        return combinedString;
+        
+    }
+    
+     public boolean addBuilding(Building b) {
+        return buildingList.add(b);       
+    }
+    
 }
--- WorkshopCode/src/workshopcode/WorkshopCode.java
+++ WorkshopCode/src/workshopcode/WorkshopCode.java
@@ -2,14 +2,25 @@
  *
  * @author bende
  */
-
 package workshopcode;
 
-
 public class WorkshopCode {
+
     public static void main(String[] args) {
+
+        Buildings yeet = new Buildings();
+        Building building = new Building("Heil", "Hitler Avenue");
+        Thermometer dank = new Thermometer("Termometer");
+        AirQualitySensor memes = new AirQualitySensor("yeet");
+        building.addSensor(dank);
+        building.addSensor(memes);
+        
+        
+        yeet.addBuilding(building);
+        
+        System.out.println(yeet.toString());
+        
     }
 
-    
-    
 }
+
