a14c3bdb61d4af78a8501e45d158e3297782190a
Niclas Ryge <nikkeryge@live.dk>
2018-11-19 14:29:35 +0100

    So far

--- WorkshopCode/src/workshopcode/Building.java
+++ WorkshopCode/src/workshopcode/Building.java
@@ -1,5 +1,7 @@
-
 package workshopcode;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  *
@@ -7,4 +9,57 @@ package workshopcode;
  */
 public class Building {
 
+    //Attributes
+    private String name;
+    private String address;
+    private final UUID uuid;
+
+    //ArrayAttributes
+    ArrayList<Sensor> sensorList = new ArrayList<>();
+    ArrayList<Actuator> actuatorList = new ArrayList<>();
+
+    //Constructor
+    public Building(String name, String address) {
+        
+        //Assigning name, address and uuid.
+        this.name = name;
+        this.address = address;
+        uuid = UUID.randomUUID();
+
+    }
+
+    //Methods
+    public boolean addSensor(Sensor s) {
+        return sensorList.add(s);       
+    }
+    
+    public boolean addActuator(Actuator a) {
+        return actuatorList.add(a);
+    }
+    
+    public void removeSensor(UUID uuid) {
+        
+    }
+    
+    
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
 }
