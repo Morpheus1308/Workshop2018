5282873be47f8f27afad24780d51035267a2f0c3
Bende Siewertsen <bende1308@gmail.com>
2018-11-19 14:26:11 +0100

    Sensor and Actuator

--- WorkshopCode/src/workshopcode/Actuator.java
+++ WorkshopCode/src/workshopcode/Actuator.java
@@ -1,10 +1,69 @@
 
 package workshopcode;
 
+import java.util.UUID;
+
 /**
  *
  * @author bende
  */
 public class Actuator {
+    private String name;
+    private UUID id;
+    private double position;
+    
+    public Actuator(String name, double position){
+      this.name = name;
+      this.position = position;
+      id = UUID.randomUUID();
+              
+    }
+   
+    public double getCurrentPos(){
+        return getPosition();
+    }
+    
+    public void setPosition(double pos){
+        position = pos;
+    }
+    
+    @Override
+    public String toString(){
+        return getName() + getId();
+    }
 
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public UUID getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the position
+     */
+    public double getPosition() {
+        return position;
+    }
 }
--- WorkshopCode/src/workshopcode/Sensor.java
+++ WorkshopCode/src/workshopcode/Sensor.java
@@ -1,10 +1,60 @@
 
 package workshopcode;
 
+import java.util.UUID;
+
 /**
  *
  * @author bende
  */
 public class Sensor {
+    private double value;
+    private String name;
+    UUID id;
+    
+    public Sensor(){
+        
+    }
+    
+    
 
+    /**
+     * @return the value
+     */
+    public double getReadout() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the desciption
+     */
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+    
+    
 }
