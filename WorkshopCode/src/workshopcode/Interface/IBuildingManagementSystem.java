ba8852ff42fc92e9b28bcd2089fc9d079f6cabc9
Niclas Ryge <nikkeryge@live.dk>
2018-11-21 13:12:07 +0100

    BMS

--- WorkshopCode/src/workshopcode/Interface/IBuildingManagementSystem.java
+++ WorkshopCode/src/workshopcode/Interface/IBuildingManagementSystem.java
@@ -18,21 +18,21 @@ public interface IBuildingManagementSystem {
      *
      * @return Map containing UUIDs of all buildings in the system along with an associated description (i.e. name)
      */
-    public Map<UUID,String> getBuildingInformation();
+    public List<String> getBuildingInformation();
 
     /**
      *
      * @param buildingId Building from which sensors are acquired
      * @return Map containing UUIDs of all sensors in building with buildingId, along with associated description of each sensor (i.e. name)
      */
-    public Map<UUID,String> getSensorInformation(UUID buildingId);
+    public List<String> getSensorInformation(UUID buildingId);
 
     /**
      *
      * @param buildingId Building from which sensors are acquired
      * @return Map containing UUIDs of all actuators in building with buildingId, along with associated description of each actuator (i.e. name)
      */
-    public Map<UUID,String> getActuatorInformation(UUID buildingId);
+    public List<String> getActuatorInformation(UUID buildingId);
     
     /**
      *
